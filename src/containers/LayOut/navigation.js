9d627250b78ddf443783f43ae9e4f7deebf237b6
Harsha <udarasandaruwan253@gmail.com>
2018-07-31 15:20:56 +0530

    set navigation

--- src/containers/LayOut/navigation.js
+++ src/containers/LayOut/navigation.js
@@ -1,20 +1,22 @@
 import React,{Component} from 'react';
-
+import { Link } from 'react-router-dom';
+import styles from "./navigation.scss";
 class Navigation extends Component {
     
     render(){
+      console.log("styles",styles)
         return(
           <div className="nuclei-navbar  navbar-wrapper ">
           <nav className="navbar navbar-inverse">
             <div className="container-fluid">
               <div className="navbar-header">
-                <a className="navbar-brand" href="#">WebSiteName</a>
+                <Link className="navbar-brand" to="/" >Gem Industry</Link>
               </div>
               <ul className="nav navbar-nav">
-                <li className="active"><a href="#">Home</a></li>
-                <li><a href="/images">Images</a></li>
-                <li><a href="/videos">Videos</a></li>
-                <li><a href="#">Page 3</a></li>
+                <li><Link to="/" className="gem-tech-nav" onClick={this.classNameChange}>Home</Link></li>
+                <li><Link to="/images">Images</Link></li>
+                <li><Link to="/videos" >Videos</Link></li>
+                <li><Link to="/" >Page 3</Link></li>
               </ul>
             </div>
           </nav>
--- src/index.js
+++ src/index.js
@@ -2,17 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
-import Navigation from './components/navigation';
+import Navigation from './containers/LayOut/navigation';
 import {store} from './redux/store';
-import Images from './components/images';
-import Videos from './components/videos';
+import Images from './containers/ImageAndVideo/images';
+import Videos from './containers/ImageAndVideo/videos';
+import Home from './containers/Home/Home'
 
 
 ReactDOM.render(<Provider store={store}>
               <BrowserRouter>
               <Switch>
                 <Navigation>
-                <Route exact path="/images" component={Images} />
+                <Route exact path="/" component={Home} />
+                <Route path="/images" component={Images} />
                 <Route path="/videos" component={Videos} />
                 </Navigation>
               </Switch>
