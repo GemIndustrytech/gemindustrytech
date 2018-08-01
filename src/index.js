ba39eccf7c3cbe784d112e9fd31e3658c16e98a5
Harsha <udarasandaruwan253@gmail.com>
2018-08-01 14:42:45 +0530

    change routs

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './index.css';
-import App from './App';
+import App from './container/App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
