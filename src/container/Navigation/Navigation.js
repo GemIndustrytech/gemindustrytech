ee9db28f766c71b0866146295706228d9bc35e60
Harsha <udarasandaruwan253@gmail.com>
2018-08-01 15:38:54 +0530

    Add Routs from navigation bar

---
+++ src/container/Navigation/Navigation.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom'
+import './Navigation.scss';
+
+class Navigation extends Component {
+  render() {
+    return (
+      <div>
+        <nav className="navbar navbar-inverse">
+          <div className="container-fluid">
+            <div className="navbar-header">
+              <Link className="navbar-brand" to="/">WebSiteName</Link>
+            </div>
+            <ul className="nav navbar-nav">
+              <li><Link to="/">Home</Link></li>
+              <li><Link to="/images">Images</Link></li>
+              <li><Link to="/videos">Videos</Link></li>
+              <li><Link to="/">Page 3</Link></li>
+            </ul>
+          </div>
+        </nav>
+        <div id="main" className="container-fluid clear-top">
+          {this.props.children}
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Navigation;
--- src/index.js
+++ src/index.js
@@ -3,8 +3,20 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import './index.css';
-import App from './container/App';
+import Navigation from './container/Navigation/Navigation';
+import Home from './container/Home/Home';
 import registerServiceWorker from './registerServiceWorker';
+import {store} from './redux/store';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+            <Provider store={store}>
+            <BrowserRouter>
+                <Navigation>
+                    <Switch>
+                        <Route expe path="/" component={Home} />
+                    </Switch>
+                </Navigation>
+                </BrowserRouter>
+            </Provider>
+    , document.getElementById('root'));
 registerServiceWorker();
